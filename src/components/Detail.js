169c97608b8017fadd0e42722097c4e5675bf368
charminggame <charminggame@hhotmail.com>
2023-02-15 15:38:36 +0700

    -Edit dat from api nobel

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-export default function Detail() {
-        const [ data,setdata ] = useState();
-        
+export default function Detail(props) {
+        const [ data,setdata ] = useState([]);
+
         return (
         <>
             <div className='flex justify-center items-center h-[90vh] w-full bg-green-300'>
-                <div>detail</div>
+                <div>{props.Datanobel}</div>
             </div>
         </>
     )
